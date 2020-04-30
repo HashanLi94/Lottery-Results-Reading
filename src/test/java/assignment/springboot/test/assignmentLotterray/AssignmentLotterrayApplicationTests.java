3fcd5d5bc90d666df3bb3d00a7111887821c80c9
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2020-04-30 12:21:56 +0530

    Create AssignmentLotterrayApplicationTests.java

---
+++ src/test/java/assignment/springboot/test/assignmentLotterray/AssignmentLotterrayApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment.springboot.test.assignmentLotterray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentLotterrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
