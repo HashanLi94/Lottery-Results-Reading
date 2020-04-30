611ab0063f3e894256f4f34cb1abbd94ed4808f6
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2020-04-30 12:21:40 +0530

    Create AssignmentLotterrayApplication.java

---
+++ src/main/java/assignment/springboot/test/assignmentLotterray/AssignmentLotterrayApplication.java
@@ -0,0 +1,19 @@
+package assignment.springboot.test.assignmentLotterray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+
+@SpringBootApplication
+public class AssignmentLotterrayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentLotterrayApplication.class, args);
+    }
+
+}
